525ad3aba44d6fd2a1dd30eaf858449738395e9b
Baruch Guerra <dolce_dmn@hotmail.com>
2017-07-26 17:04:16 -0500

    First commit. Hello world code

---
+++ meh.cpp
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <cstring>
+using namespace std; 
+int main(void){
+ 	cout<<"Hola mundo"<<endl;
+}
+ 
